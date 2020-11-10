d9778e6dba145b5db9ac2cfa5964229eba28a3dc
AnasShar <anas10895@gmail.com>
2020-11-10 19:48:13 +0300

    conecting the back end

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
     //we need to cahnge that evrey 8h ot pay 
-    baseURL:"http://7ba5df53001a.ngrok.io"
+    baseURL:"http://c750613af94a.ngrok.io"
 })
